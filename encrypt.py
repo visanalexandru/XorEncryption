9996c16d74c20f475ec57618a2332eedca40ced5
visan <alexandruvisan44@yahoo.com>
2021-11-29 19:40:39 +0200

    Indentation.

--- encrypt.py
+++ encrypt.py
@@ -6,18 +6,18 @@ def main(argv):
         print("Argument: [password] [input_path] [output_path]")
         return
 
-        chunk_size = 4096
-        secret_key = argv[0].encode()
-        encoder = XorCipher(secret_key)
+    chunk_size = 4096
+    secret_key = argv[0].encode()
+    encoder = XorCipher(secret_key)
 
-        with open(argv[1], "r") as input:
-            with open(argv[2], "wb") as output:
+    with open(argv[1], "r") as input:
+        with open(argv[2], "wb") as output:
+            chunk = input.read(chunk_size).encode()
+            output.write(encoder.apply(chunk))
+
+            while len(chunk) == chunk_size:
                 chunk = input.read(chunk_size).encode()
                 output.write(encoder.apply(chunk))
-
-                while len(chunk) == chunk_size:
-                    chunk = input.read(chunk_size).encode()
-                    output.write(encoder.apply(chunk))
 
 
 if __name__ == "__main__":
