9c2be01bc5a1b5032f01036e9214c2919dfeb4dc
visan <alexandruvisan44@yahoo.com>
2021-11-29 19:21:42 +0200

    Fix indentation.

--- encrypt.py
+++ encrypt.py
@@ -2,23 +2,23 @@ import sys
 from xor_cipher import XorCipher
 
 def main(argv):
-	if len(argv) != 3:
-		print("Argument: [password] [input_path] [output_path]")
-		return
+    if len(argv) != 3:
+        print("Argument: [password] [input_path] [output_path]")
+                return
 
-	chunk_size = 4096
-	secret_key = argv[0].encode()
-	encoder = XorCipher(secret_key)
+        chunk_size = 4096
+        secret_key = argv[0].encode()
+        encoder = XorCipher(secret_key)
 
-	with open(argv[1], "r") as input:
-		with open(argv[2], "wb") as output:
-			chunk = input.read(chunk_size).encode()
-			output.write(encoder.apply(chunk))
+        with open(argv[1], "r") as input:
+            with open(argv[2], "wb") as output:
+                chunk = input.read(chunk_size).encode()
+                        output.write(encoder.apply(chunk))
 
-			while len(chunk) == chunk_size:
-				chunk = input.read(chunk_size).encode()
-				output.write(encoder.apply(chunk))
+                        while len(chunk) == chunk_size:
+                            chunk = input.read(chunk_size).encode()
+                                output.write(encoder.apply(chunk))
 
 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    main(sys.argv[1:])
