58b36ab454765f75d18eb22cee655852abd7289b
visan <alexandruvisan44@yahoo.com>
2021-12-06 13:20:33 +0200

    Added script to get the binary file of the opponent team.

---
+++ crack/get_data.py
@@ -0,0 +1,11 @@
+fin=open("date.out","r")
+data=fin.read().strip()
+
+with open("binary.out","wb") as fout:
+
+    for x in range(0,len(data),8):
+        chunk=data[x:x+8]
+        to_int=int(chunk,2)
+        to_bytes=to_int.to_bytes(1,byteorder="big")
+        fout.write(to_bytes)
+
